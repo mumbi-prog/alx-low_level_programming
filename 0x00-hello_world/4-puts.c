ae54d6fbca085651ce0bb42500aeb484cbe3eb7e
Sylvia Mwangi <mumbi099@gmail.com>
2023-02-16 16:17:42 -0800

    Task4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - starting point for program execution
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

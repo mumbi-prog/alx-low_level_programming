79635fa21448af3b8605e6b62720da5cc0da40a7
Sylvia Mwangi <mumbi099@gmail.com>
2023-02-17 17:11:25 -0800

    Task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - print lowercase alphabet in reverse
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}

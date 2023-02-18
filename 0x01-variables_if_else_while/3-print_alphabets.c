bdc274489d97397e681378f1d05a6e442251b855
Sylvia Mwangi <mumbi099@gmail.com>
2023-02-17 16:53:06 -0800

    Task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main point of entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}

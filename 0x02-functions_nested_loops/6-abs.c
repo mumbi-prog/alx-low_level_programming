b57ff2e092b5d80e0035da92ecf1a51170c88fb2
Sylvia Mwangi <mumbi099@gmail.com>
2023-02-21 11:07:37 -0800

    Task6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - main entry point
+ * Description:a function that computes the absolute value of an integer\n
+ * @b: the int to check
+ * Return: int
+ */
+int _abs(int b)
+{
+	if (b >= 0)
+	{
+		return (b);
+	}
+	return (-b);
+}

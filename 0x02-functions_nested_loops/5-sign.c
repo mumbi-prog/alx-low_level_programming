7ef286023b2b1ff0db00124a75705f5823db8c18
Sylvia Mwangi <mumbi099@gmail.com>
2023-02-21 10:54:11 -0800

    Task5 sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - main entry point
+ * Description: function that prints the sign of a number \n
+ * Return: 1 if n>0, 0 if n == 0, -1 if n<0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}

4b0dd4c20b5554e7792aafb3616188257a86c3cf
Sylvia Mwangi <mumbi099@gmail.com>
2023-02-21 06:47:50 -0800

    Task0 putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - main entry point
+ * _putchar - prints the alphabet, in lowercase \n
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}

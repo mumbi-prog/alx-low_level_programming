7c2cf1755594027b4044e7f08bbdc1531345e822
Sylvia Mwangi <mumbi099@gmail.com>
2023-02-23 11:30:49 -0800

    Task2 mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - main entry point
+ * Description: checks for checks for a digit (0 through).
+ * @a: a - Variable
+ * @b: b - variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}

86f6d78785b903990c601db4145075f68d89ec6f
Sylvia Mwangi <mumbi099@gmail.com>
2023-02-23 05:09:38 -0800

    Task2 largestnumber

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,16 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest = b;
+	int largest;
 
-	if (a > b)
+	if (a >= b && a >= c)
 	{
-		largest = a;
+	largest = a;
 	}
-	else if (c > b)
+	else if (b >= a && b >= c)
 	{
-		largest = c;
+	largest = b;
 	}
-
+	else
+	{
+	largest = c;
+	}
 	return (largest);
 }

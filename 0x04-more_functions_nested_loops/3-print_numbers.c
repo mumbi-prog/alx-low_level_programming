bacf8ec4ed58f7ef1ba5432c843ed1021424408c
Sylvia Mwangi <mumbi099@gmail.com>
2023-02-23 11:48:47 -0800

    Task3 printnumbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - main entry point
+ * Description: print 0 - 9
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}

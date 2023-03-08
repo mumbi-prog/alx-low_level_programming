9398201dc18809348ef6c7a0d0aab6391c722845
Sylvia Mwangi <mumbi099@gmail.com>
2023-03-08 14:11:09 -0800

    Task0 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - main entry point
+ * Description: prints a string, followed by a new line
+ * @s: string to print
+ *
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}

518a70304362828fe17d022dd71503860328e699
Sylvia Mwangi <mumbi099@gmail.com>
2023-04-11 16:56:35 -0700

    Task3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,14 +10,13 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int v = 1;
+	unsigned long int x = 1;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
-	for (unsigned int i = 0; i < index; i++)
-		v <<= 1;
+	x <<= index;
+	*n |= x;
 
-	*n |= v;
 	return (1);
 }

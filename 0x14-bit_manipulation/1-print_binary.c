3ad2c0026b35ebb0f3f244d377316db9af87caef
Sylvia Mwangi <mumbi099@gmail.com>
2023-04-11 15:53:37 -0700

    Task1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,21 +6,20 @@
  *
  * Return: null
  */
+
 void print_binary(unsigned long int n)
 {
-	unsigned long int a = 1;
-	int b = 0;
-
-	do {
-		b++;
-	} while ((n >> b) > 0);
-
-	do {
-		a <<= 1;
-		_putchar(((n & a) == 0) ? '0' : '1');
-		b--;
-	} while (b >= 0);
-
-	if (n == 0)
+	if (n >> 0)
+	{
+		while (n >> 1)
+		{
+			print_binary(n >> 1);
+			break;
+		}
+	_putchar((n & 1) + '0');
+	}
+	else
+	{
 	_putchar('0');
+	}
 }
